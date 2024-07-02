2f5dcfb161fae3bc823d59a461a90bdbd7f563d3
shivu978 <131174532+shivu978@users.noreply.github.com>
2024-07-02 16:40:10 +0530

    Add files via upload

---
+++ module_13_redux/src/Counter.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './features/counterSlice';
+
+function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-2xl font-bold mb-4">Counter: {count}</div>
+      <div className="flex space-x-4 mb-4">
+        <button
+          onClick={() => dispatch(increment())}
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="px-4 py-2 bg-red-500 text-white rounded"
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => dispatch(reset())}
+          className="px-4 py-2 bg-gray-500 text-white rounded"
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
